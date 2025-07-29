1254164c03acdce4cd6d2a8f5751c6569a5f66af
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-07-29 13:26:40 +0300

    Time: 33 ms (47.85%) | Memory: 60.6 MB (90.95%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -6,18 +6,19 @@ public:
         vector<int> last(32,-1);
         vector<int> res(n);
 
-        for(int i= n - 1; i >= 0; i--) {
-            for(int b= 0; b < 32; b++)
+        for(int i = n - 1; i >= 0; i--) {
+            for(int b = 0; b < 32; b++)
                 if(nums[i] & (1 << b))
                     last[b] = i;
            
-           int  r = i;
+            int  r = i;
             for(int b = 0; b < 32; b++)
                 if(last[b] != -1)
                     r = max(r, last[b]);
             
-            res[i]=r - i + 1;
+            res[i] = r - i + 1;
         }
-       return res;
+
+        return res;
     }
 };
