57ba7a1cc28461619672e4d304b19e1bda6f4ec2
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-08-07 12:23:53 +0300

    Time: 43 ms (85.06%) | Memory: 138 MB (91.56%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int maxCollectedFruits(vector<vector<int>>& grid) {
-        int n = grid.size(), res = 0;
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int n = fruits.size(), res = 0;
 
         for (int i = 0; i < n; i++) {
-            res += grid[i][i];
+            res += fruits[i][i];
         }
 
-        // Step 2: Handle Child B and C
         for (int pass = 0; pass < 2; pass++) {
-            // On second pass, transpose grid to reuse logic
+			// Transpose the matrix
             if (pass == 1) {
                 for (int i = 0; i < n; i++) {
                     for (int j = i + 1; j < n; j++) {
-                        swap(grid[i][j], grid[j][i]);
+                        swap(fruits[i][j], fruits[j][i]);
                     }
                 }
             }
 
             vector<int> prev(n, -1), curr(n, -1);
-            prev[n - 1] = grid[0][n - 1];
+            prev[n - 1] = fruits[0][n - 1];
 
             for (int row = 1; row < n - 1; row++) {
                 fill(curr.begin(), curr.end(), -1);
                 for (int i = 0; i < n; i++) {
                     if (prev[i] < 0) continue;
                     if (i > 0)
-                        curr[i - 1] = max(curr[i - 1], prev[i] + grid[row][i - 1]);
+                        curr[i - 1] = max(curr[i - 1], prev[i] + fruits[row][i - 1]);
                     if (i < n - 1)
-                        curr[i + 1] = max(curr[i + 1], prev[i] + grid[row][i + 1]);
-                    curr[i] = max(curr[i], prev[i] + grid[row][i]);
+                        curr[i + 1] = max(curr[i + 1], prev[i] + fruits[row][i + 1]);
+                    curr[i] = max(curr[i], prev[i] + fruits[row][i]);
                 }
                 swap(prev, curr);
             }
