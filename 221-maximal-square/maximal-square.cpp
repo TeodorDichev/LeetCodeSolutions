113f8b85172020c55925345180f8f349c06547ee
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-08-20 12:36:50 +0300

    Time: 3 ms (92.97%) | Memory: 27.4 MB (97.72%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
+int maximalSquare(vector<vector<char>>& matrix) {
     int m = matrix.size();
     if (m == 0) return 0;
     int n = matrix[0].size();
 
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-    int maxSquare = 0;
+    vector<int> dp(n + 1, 0);
+    int maxSquare = 0, prev = 0;
 
     for (int i = 0; i < m; i++) {
+        prev = 0;
         for (int j = 0; j < n; j++) {
+            int temp = dp[j+1];
+
             if (matrix[i][j] == '1') {
-                dp[i+1][j+1] = 1 + min({dp[i][j], dp[i][j+1], dp[i+1][j]});
-                maxSquare = max(maxSquare, dp[i+1][j+1]);
+                dp[j+1] = 1 + min({dp[j], dp[j+1], prev});
+                maxSquare = max(maxSquare, dp[j+1]);
+            } 
+            else {
+                dp[j+1] = 0;
             }
+
+            prev = temp;
         }
     }
 
