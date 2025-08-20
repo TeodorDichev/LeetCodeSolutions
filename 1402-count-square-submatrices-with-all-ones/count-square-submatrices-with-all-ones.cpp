777dfdfdfa23dbe80eea5ef7e1dc0eb776b70c8e
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-08-20 13:08:58 +0300

    Time: 11 ms (28.49%) | Memory: 27.5 MB (92.22%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -5,7 +5,7 @@ public:
     if (m == 0) return 0;
     int n = matrix[0].size();
 
-    int res = 0;//, prev = 0;
+    int res = 0;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if (matrix[i][j]) {
