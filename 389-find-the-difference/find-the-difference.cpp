2d4233f52ec350652ed913fdf0de7b3517f77f51
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-10-20 12:37:25 +0300

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.11%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+char findTheDifference(string s, string t) {
+    vector<int> counts(26, 0);
+    for(auto& c:s)
+    {
+        counts[c-'a'] += 1;
+    }
+
+    for(auto& c:t)
+    {
+        if(--counts[c-'a'] < 0)
+        {
+            return c;
+        } 
+    }
+    
+    return '\0';
+}
+};
