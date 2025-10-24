a039b2ecbaeb366e16218763b2c34fadc0ca1c60
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-10-24 13:58:08 +0300

    Time: 545 ms (26.63%) | Memory: 254.8 MB (27.14%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -1,39 +1,23 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution {
 public:
-    int nextBeautifulNumber(int n) {
-        // brute force
-
-        for(int i = n + 1; i < INT_MAX; i++) {
-            unordered_map<int, int> map;
-            bool mark = false;
-            for(int j = i; j > 0; j /= 10) {
-                int d = j % 10;
-
-                if(d == 0) {
-                    mark = true;
-                    break;
-                }
-                    
-                map[d]++;
-            }
-
-            if(mark) 
-                continue;
-                
-            bool all = true;
-            for(int k = 1; k < 10; k++) {
-                if(map[k] != k && map[k] != 0) {
-                    all = false;
-                    break;
-                }
-            }
-
-            if(all) return i;
+    bool check(int x) {
+        vector<int> cnt(10);
+        while(x > 0) {
+            cnt[x % 10]++;
+            x /= 10;
         }
+
+        for(int d = 0; d < 10; d++)
+            if(cnt[d] > 0 && cnt[d] != d)
+                return false;
+        
+        return true;
+    }
+
+    int nextBeautifulNumber(int n) {
+        for(int i = n + 1; i <= 1224444; i++)
+            if(check(i))
+                return i;
 
         return -1;
     }
