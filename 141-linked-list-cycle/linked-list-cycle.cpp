7931e1849ff1febf371da1f22f66fa004787323d
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-11-03 20:29:40 +0200

    Time: 8 ms (80.09%) | Memory: 11.8 MB (79.68%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+
+        ListNode *slow = head, *fast = head->next;
+
+        while(slow && fast) {
+            if(slow == fast)
+                return true;
+                
+            slow = slow->next;
+            fast = fast->next;
+            if(fast) fast = fast->next;
+        }
+
+        return false;
+    }
+};
