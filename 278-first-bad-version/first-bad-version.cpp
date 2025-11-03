d363fb102d9074ea24004bd291286512a83f468a
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-11-03 18:54:13 +0200

    Time: 2 ms (56.03%) | Memory: 8 MB (37.84%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -6,10 +6,10 @@ public:
     int firstBadVersion(int n) {
         int low = 1;
         int high = n;
-        while(low < high) {
+        while(low <= high) {
             int mid = low + (high - low) / 2;
             if(isBadVersion(mid)) 
-                high = mid;
+                high = mid - 1;
             else
                 low = mid + 1;
         }
