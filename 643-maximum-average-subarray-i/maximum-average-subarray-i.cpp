b8a6bd7017b85a643b7d8be571f4e3e37896711e
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-11-16 22:49:15 +0200

    Time: 16 ms (5.10%) | Memory: 120 MB (14.17%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        queue<int> q; int maxSum = 0;
+        for(int i = 0; i < k; i++) {
+            q.push(nums[i]);
+            maxSum += nums[i];
+        }
+
+        int currSum = maxSum;
+        for(int i = k; i < nums.size(); i++) {
+            currSum += nums[i] - q.front();
+            q.pop();
+            q.push(nums[i]);
+            if(currSum > maxSum) {
+                maxSum = currSum;
+            }
+        }
+
+        return (1.0 * maxSum) / k;
+    }
+};
