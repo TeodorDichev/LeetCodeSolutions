c3a4149d2ca45429051f1143196eaee83cd77ad3
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-11-17 15:05:44 +0200

    Time: 0 ms (100.00%) | Memory: 18.5 MB (29.41%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,36 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p || !q) 
-            return p == q;
-        if (p->val != q->val) 
+     bool isSymmetricHelp(TreeNode* left , TreeNode* right){
+        if( left == nullptr || right == nullptr){
+            return left == right;
+        }
+        if( left->val != right->val){
             return false;
-
-        return isSameTree(p->left, q->left) &&
-                 isSameTree(p->right, q->right);
-    }
-
-    void invertTreeRec(TreeNode* node) {
-        if(!node)
-            return;
-        swap(node->left, node->right);
-        invertTreeRec(node->left);
-        invertTreeRec(node->right);
+        }
+        return (isSymmetricHelp(left->left , right->right) && isSymmetricHelp(left->right , right->left));
     }
 
     bool isSymmetric(TreeNode* root) {
-        invertTreeRec(root->right);
-        return isSameTree(root->left, root->right);
+        return ( root == nullptr || isSymmetricHelp(root->left , root->right));
     }
 };
