54cf003b5dd3a73ed2eea724a5326bb4034a4bbc
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-11-30 21:14:07 +0200

    Time: 44 ms (97.68%) | Memory: 173 MB (82.72%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,31 @@
+class LRUCache {
+private:
+    int cap;
+    list<pair<int,int>> cache;
+    unordered_map<int, list<pair<int,int>>::iterator> mp;
+
+public:
+    LRUCache(int capacity) : cap(capacity) {}
+    
+    int get(int key) {
+        if (mp.find(key) == mp.end()) return -1;
+        cache.splice(cache.begin(), cache, mp[key]);
+        return mp[key]->second;
+    }
+    
+    void put(int key, int value) {
+        if (mp.find(key) != mp.end()) {
+            mp[key]->second = value;
+            cache.splice(cache.begin(), cache, mp[key]);
+        } else {
+            if (cache.size() == cap) {
+                auto last = cache.back();
+                mp.erase(last.first);
+                cache.pop_back();
+            }
+
+            cache.push_front({key, value});
+            mp[key] = cache.begin();
+        }
+    }
+};
