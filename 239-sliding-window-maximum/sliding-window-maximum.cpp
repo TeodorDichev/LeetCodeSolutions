3681431240bb340c4074a24361fe2890c088e01c
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2025-12-20 11:46:42 +0200

    Time: 63 ms (17.48%) | Memory: 153.1 MB (17.37%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> result;
-        deque<int> window;
-
-        for(int i = 0; i < k; i++) {
-            while(!window.empty() && nums[window.back()] < nums[i]) {
-                window.pop_back();
+        priority_queue<pair<int,int>> pq;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+            
+            if (i >= k - 1) {
+                while (pq.top().second <= i - k) {
+                    pq.pop();
+                }
+                result.push_back(pq.top().first);
             }
-            window.push_back(i);
         }
-
-        result.push_back(nums[window.front()]);
-
-        for(int i = k; i < nums.size(); i++) {
-            if(window.front() == i - k) {
-                window.pop_front();
-            }
-
-            while(!window.empty() && nums[window.back()] < nums[i]) {
-                window.pop_back();
-            }
-
-            window.push_back(i);
-            result.push_back(nums[window.front()]);
-        }
-
+        
         return result;
     }
 };
