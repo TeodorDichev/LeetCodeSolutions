719642279dd414a0342011ca7cf3e4b023a278bd
Teodor Dichev <96772763+TeodorDichev@users.noreply.github.com>
2026-04-18 11:18:32 +0300

    Time: 0 ms (100.00%) | Memory: 8.6 MB (30.08%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int revNum(int x) {
+        int y = 0;
+        while (x > 0) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+
+        return y;
+    }
+
+public:
+    int mirrorDistance(int n) {
+        return abs(n - revNum(n));
+    }
+};
